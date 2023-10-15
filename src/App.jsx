52a53db7a5a07a59aa28b04193d40d98618163a1
Dominque Piffardi <domiskp@gmail.com>
2023-10-15 19:56:44 -0300

    Dominique: Agregué Footer

--- src/App.jsx
+++ src/App.jsx
@@ -5,43 +5,34 @@ import { BaseColaboradores } from "./utils/BaseColaboradores";
 import ListadoColaboradores from "./components/Listado";
 import FormularioAgregarColaborador from "./components/Formulario";
 import BusquedaColaboradores from "./components/BusquedaColaboradores";
+import Footer from "./components/Footer";
 
 function App() {
-	const [colaboradores, setColaboradores] = useState(BaseColaboradores);
-	const [resultadosBusqueda, setResultadosBusqueda] = useState([]);
+  const [colaboradores, setColaboradores] = useState(BaseColaboradores);
+  const [resultadosBusqueda, setResultadosBusqueda] = useState([]);
 
-	const agregarColaborador = (nuevoColaborador) => {
-		// Agrega el nuevo colaborador a la lista completa
-		setColaboradores([...colaboradores, nuevoColaborador]);
+  const agregarColaborador = (nuevoColaborador) => {
+    // Agrega el nuevo colaborador a la lista completa
+    setColaboradores([...colaboradores, nuevoColaborador]);
 
-		// Actualiza los resultados de búsqueda con la lista completa
-		setResultadosBusqueda([...colaboradores, nuevoColaborador]);
-	};
+    // Actualiza los resultados de búsqueda con la lista completa
+    setResultadosBusqueda([...colaboradores, nuevoColaborador]);
+  };
 
-	const handleResultadoBusqueda = (resultados) => {
-		setResultadosBusqueda(resultados);
-	};
+  const handleResultadoBusqueda = (resultados) => {
+    setResultadosBusqueda(resultados);
+  };
 
-	return (
-		<div>
-			<BusquedaColaboradores
-				colaboradores={colaboradores}
-				onResultadoBusqueda={handleResultadoBusqueda}
-			/>
-			<div className="App">
-				<ListadoColaboradores
-					datos={
-						resultadosBusqueda.length > 0
-							? resultadosBusqueda
-							: colaboradores
-					}
-				/>
-				<FormularioAgregarColaborador
-					onAgregarColaborador={agregarColaborador}
-				/>
-			</div>
-		</div>
-	);
+  return (
+    <div>
+      <BusquedaColaboradores colaboradores={colaboradores} onResultadoBusqueda={handleResultadoBusqueda} />
+      <div className="App">
+        <ListadoColaboradores datos={resultadosBusqueda.length > 0 ? resultadosBusqueda : colaboradores} />
+        <FormularioAgregarColaborador onAgregarColaborador={agregarColaborador} />
+      </div>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
